01436bfa36fe6bb78e09a272dd45cc3516c730d1
Breanne <BreMarotta@gmail.com>
2022-06-25 12:59:12 -0400

    initial navigation setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navigation from "./features/settings/Navigation";
 import Home from "./features/settings/Home";
 
 function App() {
@@ -14,11 +15,13 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navigation />
         <Switch>
           <Route path="/testing">
             <h1>Test Route</h1>
           </Route>
-          <Route exact path="/" >{<Home />}
+          <Route exact path="/" >
+            <Home />
             {/* <h1>Page Count: {count}</h1> */}
           </Route>
         </Switch>
---
+++ client/src/features/settings/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink, useNavigation } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+        <NavLink to="/" className="">Home</NavLink>
+        <NavLink to="/items" className="">Itmes</NavLink>
+        <NavLink to="/repairs" className="">Repairs</NavLink>
+        <NavLink to="/testing" className="">Test Route</NavLink>
+    </div>
+  )
+}
+
+export default Navigation
