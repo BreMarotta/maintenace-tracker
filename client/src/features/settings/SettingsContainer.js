acd557ed2e47c2ea2a4a0ae9e5c47333a02e7ef7
Breanne <BreMarotta@gmail.com>
2022-07-03 16:20:50 -0400

    Add checkboxes on settings to display and hide forms

--- client/src/features/settings/SettingsContainer.js
+++ client/src/features/settings/SettingsContainer.js
@@ -1,20 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SettingsForm from './SettingsForm'
 import DesignForm from '../designs/DesignForm'
 import PersonForm from '../people/PersonForm'
 
 const SettingsContainer = ({ toggle }) => {
+  const [showSettings, setShowSettings] = useState(false)
+  const [showPerson, setShowPerson] = useState(false)
+  const [showDesign, setShowDesign] = useState(false)
+
+  const toggleSettings = () => {
+    setShowSettings(!showSettings)
+  }
+
+  const displaySettings = showSettings == true ? <SettingsForm /> : ""
+
+  const toggleDesign = () => {
+    setShowDesign(!showDesign)
+  }
+
+  const displayDesign = showDesign == true ? <DesignForm toggle={toggle}/> : ""
+
+  const togglePerson = () => {
+    setShowPerson(!showPerson)
+  }
+
+  const displayPerson = showPerson == true ? <PersonForm toggle={toggle}/> : ""
+
   return (
-    <div>
-      <SettingsForm />
+    <div> 
+      <h3>Settings:  </h3>
       <hr/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <PersonForm toggle={toggle}/>
+      <label>User Settings: </label>
+        <input
+          type="checkbox"
+          checked={showSettings}
+          onChange={toggleSettings}/>
+      {displaySettings}
       <hr/>
-      <DesignForm toggle={toggle}/>
+      <label>Personalize Design: </label>
+        <input
+            type="checkbox"
+            checked={showDesign}
+            onChange={toggleDesign}/>
+      {displayDesign}
+      <hr/>
+      <label>Add : </label>
+        <input
+            type="checkbox"
+            checked={showPerson}
+            onChange={togglePerson}/>
+      {displayPerson}
+      
         
     </div>
     
