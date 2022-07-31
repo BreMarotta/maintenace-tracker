fcd9b767299fe663d8b4078718be2e3e0625a350
Breanne <BreMarotta@gmail.com>
2022-07-31 17:36:21 -0400

    Grid and card layout working for parts of item

--- client/src/Styles/Cards.style.js
+++ client/src/Styles/Cards.style.js
@@ -2,45 +2,57 @@ import styled from 'styled-components';
 
 export const Grid = styled.div`
     display: inline-grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(5, 1fr);
     grid-template-rows: repeat(2, 1fr);
+    margin-top: 25px;
     grid-gap: 10px;
     padding: 15;
 `
 
 export const Card = styled.div`
     display: inline-grid;
-    grid-template-columns: 60% 35%;
-    grid-template-rows: 75% 25%;
-    grid-gap: 5%;
-    /* flex-flow: row; */
+    grid-template-columns: 100%;
+    grid-template-rows: 40% 20% 15% 15%;
+    grid-row-gap: 5%;
+    grid-column-gap: 7%;
     border: ${(props) => props.accent} solid 2px;
     border-radius: 5px;
     width: 100%;
+    height: 100%;
     box-shadow: 3px 4px slategray;
+
 
     & img {
         display: flex;
-        grid-row: 1/3;
         grid-column:1/2;
         border: solid black;
         max-height: 100%;
-        max-width: 100%;
+        max-width: 85%;
         object-fit: cover;
+        margin-top: 10px;
+        margin-left: auto;
+        margin-right: auto;
     }
     & h3 {
         display: flex;
-        grid-column: 2/2;
-        grid-row: 1/2;
+        font-size: 60%;
+        grid-row: 2/3;
         max-width: 100%;
         object-fit: cover;
-        /* margin-top: -15px; */
+        margin-left: auto;
+        margin-right: auto;
+    }
+    & h1 {
+        font-size: 150%;
     }
     & button {
         display: flex;
-        grid-column: 2/2;
-        grid-row: 2/2;
+        /* grid-column: 2/2; */
+        grid-row: 4/4;
         /* flex: 3; */
+        padding-bottom: 15px;
+        margin-left: 5px;
+        margin-right: 5px;
     }
 
 `
--- client/src/features/parts/Part.js
+++ client/src/features/parts/Part.js
@@ -2,37 +2,36 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDesign } from '../designs/useDesign'
 import PartForm from './PartForm';
-import { Card, Grid } from '../../Styles/Cards.style';
-import { Button, Banner } from '../../Styles/Styled';
+import { Card } from '../../Styles/Cards.style';
+import { Button } from '../../Styles/Styled';
 
 const Part = ({ part }) => {
     const design = useDesign()
     const [showForm, setShowForm] = useState(false)
     const [showDetails, setShowDetails] = useState(false)
 
-    console.log(part)
     const toggleForm = () => {
         setShowForm(!showForm)
     }
 
-    const toggleDetails = () => {
-        setShowDetails(!showDetails)
-    }
-
-    const details = 
+    const details = showDetails ? 
         <div>
-            {part.model ? <span>Model: {part.model}</span> : ""}
+            {part.name}
+            <hr/>
+            {part.model ? <span>Model: {part.model} <br/></span> : ""}
             {part.price ? <span>Price: ${part.price}<br/></span> : "" }
-            {part.details ? <span>Additional Information: {part.details}</span> : ""}
+            {part.details ? <span><hr/> {part.details}</span> : ""}
         </div>
+        :
+        <h1>{part.name}</h1>
 
     const buttonText = showForm == true ? "Hide Form" : "Update"
 
     const displayPart = showForm == true ? <PartForm toggle={toggleForm} part={part}/> : 
-        <Card>  
+        <Card onMouseEnter={() => setShowDetails(true)} onMouseLeave={() => setShowDetails(false)}>  
             <img src={part.img} alt={part.name} />
             <br/>
-            <h3>{part.name}</h3>
+            <h3>{details}</h3>
             {showDetails}
             <Button backgroundColor={design.accent} onClick={toggleForm}>{buttonText}</Button>           
         </Card>
