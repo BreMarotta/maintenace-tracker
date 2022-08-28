11505472b984029743b2580f674f97ce6f1ceb4b
Breanne <BreMarotta@gmail.com>
2022-08-27 21:56:26 -0400

    Filter people to only show current on toggle

--- client/src/features/people/People.js
+++ client/src/features/people/People.js
@@ -3,22 +3,32 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Person from '../people/Person'
 import { DisperseInfo } from '../../Disperse'
-import { StyledBackground, Button, StyledLink } from '../../Styles/Styled'
+import { StyledBackground, Button, StyledLink, EditButton } from '../../Styles/Styled'
 import { useDesign } from '../designs/useDesign'
 import Login from '../settings/Login'
 import { PersonGrid } from '../../Styles/Cards.style'
+
 
 
 const People = () => {
   const { loggedIn } = useContext(DisperseInfo)
   const design = useDesign()
   const people = useSelector(state => state.people.people)
+  const [filterActive, setFilterActive] = useState(false)
 
-  const display = people.map(p => <Person key={p.id} person={p}/>)
+  const fPeople = people.filter(p => p.current === true)
+
+  console.log(fPeople)
+
+  const display = filterActive ? fPeople.map(p => <Person key={p.id} person={p}/>) : people.map(p => <Person key={p.id} person={p}/>)
+
+  const aText = filterActive ? "Show All" : "Show Active Only"
 
   if (loggedIn) {
     return (
     <StyledBackground backgroundColor={design.background}>
+      <EditButton backgroundColor={design.accent} side="right" onClick={() => setFilterActive(!filterActive)}>{aText}</EditButton>
+      <br/>
         <Link to='/people/new'><StyledLink><Button backgroundColor={design.main}>Add Person</Button></StyledLink></Link>
         <br/>
         <PersonGrid>
