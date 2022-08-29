fbeb9ff93cf20147417b50063c69ca87c079b925
Breanne <BreMarotta@gmail.com>
2022-08-29 10:45:26 -0400

    attempt to get heroku working again

--- client/src/features/repairs/Repair.js
+++ client/src/features/repairs/Repair.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { RepairCard } from '../../Styles/Cards.style';
-import { Button, StyledLink, EditButton } from '../../Styles/Styled';
+import { Button, StyledLink } from '../../Styles/Styled';
 import { useSelector } from 'react-redux';
-import { deleteRepFront, deleteRepair } from './repairsSlice';
 
 const Repair = (props) => {
   const people = useSelector((state) =>state.people.people)
   const items = useSelector((state) => state.items.items)
   const parts = useSelector((state) => state.parts.parts)
-  const [del, setDel] = useState(false)
-  const history = useHistory()
 
   const person  = people.find(y => y.id === props.repair.person_id)
 
@@ -31,30 +28,41 @@ const Repair = (props) => {
   //   })
   // }
 
-  const display = del ?
-  <div>
-    {/* <p>Are you sure you want to delete <strong>{props.repair.title}</strong>?</p> 
-    <Button backgroundColor="black" onClick={() => setDel(false)}>No</Button>
-    <Button backgroundColor="black" onClick={handleDelete}>Confirm Delete</Button> */}
-  </div>
-  :
-  <div>
-    <EditButton background={person.current == false || null  ? "Gainsboro" : "white"} accent="whitesmoke" side="right" onClick={() => setDel(!del)}>🗑️</EditButton>
-    <h2>{p.name ? <div><span>{p.name} - {thing.name}</span></div> : thing.name}</h2>
-    <h4>{props.repair.title == "" ? "" : `${props.repair.title}`}</h4>
-    <div>
-      {props.repair.complete == false || null ? "Ongoing Repair"  : complete}
-      <br/>
-      {props.repair.cost == null ? "" : `Cost: $${props.repair.cost}`}
-    </div>
+  // const display = del ?
+  // <div>
+  //   {/* <p>Are you sure you want to delete <strong>{props.repair.title}</strong>?</p> 
+  //   <Button backgroundColor="black" onClick={() => setDel(false)}>No</Button>
+  //   <Button backgroundColor="black" onClick={handleDelete}>Confirm Delete</Button> */}
+  // </div>
+  // :
+  // <div>
+  //   {/* <EditButton background={person.current == false || null  ? "Gainsboro" : "white"} accent="whitesmoke" side="right" onClick={() => setDel(!del)}>🗑️</EditButton> */}
+  //   <h2>{p.name ? <div><span>{p.name} - {thing.name}</span></div> : thing.name}</h2>
+  //   <h4>{props.repair.title == "" ? "" : `${props.repair.title}`}</h4>
+  //   <div>
+  //     {props.repair.complete == false || null ? "Ongoing Repair"  : complete}
+  //     <br/>
+  //     {props.repair.cost == null ? "" : `Cost: $${props.repair.cost}`}
+  //   </div>
 
-    <h5>{person ? `Repair by: ${person.name}` : ""}</h5>
-    <Button backgroundColor={person.color ? person.color : "black"}><Link to={`/repairs/${props.repair.id}/update`} ><StyledLink>Update Repair Info</StyledLink></Link></Button>
-  </div>
+  //   <h5>{person ? `Repair by: ${person.name}` : ""}</h5>
+  //   <Button backgroundColor={person.color ? person.color : "black"}><Link to={`/repairs/${props.repair.id}/update`} ><StyledLink>Update Repair Info</StyledLink></Link></Button>
+  // </div>
 
   return ( 
     <RepairCard  accent={person ? person.color : "black"} background={person.current == false || null  ? "Gainsboro" : "white"}>
-      {display}
+      <div>
+        <h2>{p.name ? <div><span>{p.name} - {thing.name}</span></div> : thing.name}</h2>
+        <h4>{props.repair.title == "" ? "" : `${props.repair.title}`}</h4>
+        <div>
+          {props.repair.complete == false || null ? "Ongoing Repair"  : complete}
+          <br/>
+          {props.repair.cost == null ? "" : `Cost: $${props.repair.cost}`}
+        </div>
+
+        <h5>{person ? `Repair by: ${person.name}` : ""}</h5>
+        <Button backgroundColor={person.color ? person.color : "black"}><Link to={`/repairs/${props.repair.id}/update`} ><StyledLink>Update Repair Info</StyledLink></Link></Button>
+      </div>
     </RepairCard>
   )
 }
